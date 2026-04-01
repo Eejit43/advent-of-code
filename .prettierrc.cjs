9654e5eb6efe8164ce204fc6f6a2355d6f54cd80
Will <76887639+Eejit43@users.noreply.github.com>
2026-03-31 23:29:37 -0400

    chore: resolve eslint errors

--- .prettierrc.cjs
+++
@@ -1,6 +0,0 @@
-const prettierConfig = require('@eejit/prettier-config');
-
-module.exports = {
-    ...prettierConfig,
-    plugins: prettierConfig.plugins.filter((plugin) => plugin !== 'prettier-plugin-css-order'),
-};
